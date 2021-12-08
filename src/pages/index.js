95c82731eea83d0a4c9f4b0676866eeed554cc59
Harsh Bharucha <harsh@shadesofweb.com>
2021-12-08 18:36:52 +0530

    Hello World!

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <div><strong> Hello World! </strong></div><br/>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
